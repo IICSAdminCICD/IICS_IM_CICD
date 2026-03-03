2a3a5e4d3bfae08ae8220f4cb65b50f832542cc2
IICSAdminCICD <dl-GlobalMasterIICSAdmin@ppg.com>
2026-03-03 14:42:00 +0100

    GovOps CICD: Updated python file updated

--- scripts/infa_update_and_test.py
+++ scripts/infa_update_and_test.py
@@ -56,35 +56,35 @@ if r.status_code != 200:
 request_json = r.json()
 
 # Only get Mapping Tasks
-r_filtered = [x for x in request_json['changes'] if ( x['type'] == 'MTT') ]
-
+#r_filtered = [x for x in request_json['changes'] if ( x['type'] == 'MTT') ]
+#
 # This loop runs tests for each one of the mapping tasks
-for x in r_filtered:
-    BODY = {"@type": "job","taskId": x['appContextId'],"taskType": "MTT"}
-    t = requests.post(URL + "/api/v2/job/", headers = HEADERS_V2, json = BODY )
-
-    if t.status_code != 200:
-        print("Exception caught: " + t.text)
-        sys.exit(99)
-
-    test_json = t.json()
-    PARAMS = "?runId=" + str(test_json['runId'])
-    #"?taskId=" + test_json['taskId']
-
-    STATE=0
-    
-    while STATE == 0:
-        time.sleep(60)
-        a = requests.get(URL + "/api/v2/activity/activityLog" + PARAMS, headers = HEADERS_V2)
-        
-        activity_log = a.json()
-
-        STATE = activity_log[0]['state']
-
-    if STATE != 1:
-        print("Mapping task: " + activity_log[0]['objectName'] + " failed. ")
-        sys.exit(99)
-    else:
-        print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully. ")
+#for x in r_filtered:
+#    BODY = {"@type": "job","taskId": x['appContextId'],"taskType": "MTT"}
+#    t = requests.post(URL + "/api/v2/job/", headers = HEADERS_V2, json = BODY )
+#
+#    if t.status_code != 200:
+#        print("Exception caught: " + t.text)
+#        sys.exit(99)
+#
+#    test_json = t.json()
+#    PARAMS = "?runId=" + str(test_json['runId'])
+#    #"?taskId=" + test_json['taskId']
+#
+#    STATE=0
+#    
+#    while STATE == 0:
+#        time.sleep(60)
+#        a = requests.get(URL + "/api/v2/activity/activityLog" + PARAMS, headers = HEADERS_V2)
+#        
+#        activity_log = a.json()
+#
+#        STATE = activity_log[0]['state']
+#
+#    if STATE != 1:
+#        print("Mapping task: " + activity_log[0]['objectName'] + " failed. ")
+#        sys.exit(99)
+#    else:
+#        print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully. ")
 
 requests.post(URL + "/public/core/v3/logout", headers = HEADERS)
