9f18a1736e1df033ce6d60c865cd2c3c405f88ef
IICSAdminCICD <dl-GlobalMasterIICSAdmin@ppg.com>
2026-03-11 17:37:07 +0100

    Update infa_login.py

--- CICD/scripts/infa_login.py
+++ CICD/scripts/infa_login.py
@@ -5,8 +5,8 @@ URL = os.environ['IICS_LOGIN_URL']
 DEV_USERNAME = os.environ['DEV_IICS_USERNAME']
 DEV_PASSWORD = os.environ['DEV_IICS_PASSWORD']
 
-UAT_USERNAME = os.environ['UAT_IICS_USERNAME']
-UAT_PASSWORD = os.environ['UAT_IICS_PASSWORD']
+QA_USERNAME = os.environ['QA_IICS_USERNAME']
+QA_PASSWORD = os.environ['QA_IICS_PASSWORD']
 
 BODY = {"username": DEV_USERNAME, "password": DEV_PASSWORD}
 
@@ -15,21 +15,21 @@ r = requests.post(url=URL, json=BODY)
 if r.status_code != 200:
     raise Exception("Login DEV failed: " + r.text)
 
-UAT_BODY = {"username": UAT_USERNAME, "password": UAT_PASSWORD}
+QA_BODY = {"username": QA_USERNAME, "password": QA_PASSWORD}
 
-u = requests.post(url=URL, json=UAT_BODY)
+u = requests.post(url=URL, json=QA_BODY)
 
 if u.status_code != 200:
-    raise Exception("Login UAT failed: " + u.text)
+    raise Exception("Login QA failed: " + u.text)
 
 data = r.json()
 uat_data = u.json()
 
 sessionId = data['userInfo']['sessionId']
-uat_sessionId = uat_data['userInfo']['sessionId']
+qa_sessionId = qa_data['userInfo']['sessionId']
 
 print("DEV session obtained")
-print("UAT session obtained")
+print("QA session obtained")
 
 print(f"##vso[task.setvariable variable=sessionId;isOutput=true]{sessionId}")
-print(f"##vso[task.setvariable variable=uat_sessionId;isOutput=true]{uat_sessionId}")
+print(f"##vso[task.setvariable variable=uat_sessionId;isOutput=true]{qa_sessionId}")
