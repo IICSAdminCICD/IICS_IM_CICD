38da0f03b2cfda7e41d1aa967e01aa11e4b4baf0
IICSAdminCICD <dl-GlobalMasterIICSAdmin@ppg.com>
2026-04-16 13:00:11 +0200

    Create runtime_mapping.py

---
+++ CICD/scripts/runtime_mapping.py
@@ -0,0 +1,9 @@
+runtime_mapping:
+  - DEV:
+      name: SDCAMDMSA01
+      id: 010T4725000000000004
+      federatedId: 9ew81IGCjUshOwChB1zgWT
+    TARGET:
+      name: AG_IICS_QA_CDI
+      id: 010T4G2500000000000G
+      federatedId: fCPnTBJj0qvloh3kEBbhA3
